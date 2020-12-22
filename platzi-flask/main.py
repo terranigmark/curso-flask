14da7855f6141d3b4f52482c453dcde079b3fc97
Hector Vega <linnk99@gmail.com>
2020-12-22 10:43:37 -0500

    Flash message for registering added

--- platzi-flask/main.py
+++ platzi-flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -49,6 +49,8 @@ def hello():
     if login_form.validate_on_submit():
         username = login_form.username.data
         session['username'] = username
+
+        flash('Username successfully registered.')
 
         return redirect(url_for('index'))
 
