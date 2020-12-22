25c1f7a0b8ffbfa8f5bf6be6ab36fc4833672f16
Hector Vega <linnk99@gmail.com>
2020-12-22 11:17:13 -0500

    Tests for app existance, test mode, index redirects, hello get and post added

--- platzi-flask/main.py
+++ platzi-flask/main.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
 
 app = Flask(__name__, template_folder='./templates')
 bootsrap = Bootstrap(app)
@@ -15,6 +16,11 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404)
 def not_found(error):
