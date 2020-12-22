9dea544e58127dec8c339a0336c799e4dcddf3ee
Hector Vega <linnk99@gmail.com>
2020-12-21 20:03:24 -0500

    Redirect using cookies and getting user's IP

---
+++ platzi-flask/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, make_response, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+
+    return f'Hello World Platzi, your IP is {user_ip}'
