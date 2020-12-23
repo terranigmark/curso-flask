937c2edceca82464275d18dfdddb548a67533167
Hector Vega <linnk99@gmail.com>
2020-12-23 14:16:55 -0500

    Blueprint almost finished for login

--- platzi-flask/app/auth/views.py
+++ platzi-flask/app/auth/views.py
@@ -1,10 +1,20 @@
-from flask import render_template
+from flask import render_template, session, redirect, url_for
 from . import auth
 from app.forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    login_form = LoginForm()
     context = {
         'login_form': LoginForm(),
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Username successfully registered.')
+
+        return redirect(url_for('index'))
+
     return render_template('login.html', **context)
--- platzi-flask/tests/test_base.py
+++ platzi-flask/tests/test_base.py
@@ -3,6 +3,7 @@ from flask import current_app, url_for
 
 from main import app
 
+
 class MainTest(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
@@ -27,13 +28,9 @@ class MainTest(TestCase):
         self.assert200(response)
 
     def test_hello_post(self):
-        fake_form = {
-            'username': 'fake',
-            'password': 'fake-password'
-        }
-        response = self.client.post(url_for('hello'), data=fake_form)
+        response = self.client.post(url_for('hello'))
 
-        self.assertRedirects(response, url_for('index'))
+        self.assertTrue(response.status_code, 405)
 
     def test_auth_blueprint_exists(self):
         self.assertIn('auth', self.app.blueprints)
@@ -44,6 +41,15 @@ class MainTest(TestCase):
         self.assert200(response)
 
     def test_auth_login_template(self):
-        response = self.client.get(url_for('auth.login'))
+        self.client.get(url_for('auth.login'))
 
         self.assertTemplateUsed('login.html')
+
+    def test_auth_login_post(self):
+        fake_form = {
+            'username': 'fake',
+            'password': 'fake-password'
+        }
+
+        response = self.client.post(url_for('auth.login'), data=fake_form)
+        self.assertRedirects(response, url_for('index'))
